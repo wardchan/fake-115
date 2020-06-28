ac945b07cd2c3200d3eb9a6a7ef4ea2170ad2120
Ward <i@wardchan.com>
2020-06-28 20:50:03 +0800

    feat(electron): support show download list

--- electron/src/index.js
+++ electron/src/index.js
@@ -1,9 +1,16 @@
-const { app, BrowserWindow, session } = require('electron')
-const path = require('path')
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  session,
+  dialog,
+} = require('electron')
 
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit()
 }
+
+const downloads = []
 
 const cookies = [
   {
@@ -98,29 +105,89 @@ const cookies = [
   },
 ]
 
+const menuTemplate = [
+  {
+    label: '操作',
+    submenu: [
+      {
+        label: '下载任务管理',
+        async click() {
+          await dialog.showMessageBox({
+            type: 'none',
+            message: '下载任务列表（暂时只支持显示任务列表，后续会支持下载任务的操作，例如暂停或取消）',
+            detail: `${downloads.map(d => {
+              return `文件名：\n${d.filename}\n进度：${d.progress}\n状态：${d.status}`
+            }).join('\n---------------------\n')}`,
+          })
+        },
+      }
+    ]
+  }
+]
+
 const createWindow = async () => {
+  if (process.platform === 'darwin') {
+    menuTemplate.unshift({
+      label: app.getName(),
+      submenu: [
+        {
+          label: '退出',
+          click() {
+            app.quit()
+          },
+        },
+      ],
+    })
+  }
+
+  const appMenu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(appMenu)
+
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 800,
   })
 
   for (let i = 0, length = cookies.length; i < length; i ++) {
     const cookie = cookies[i]
     try {
       await session.defaultSession.cookies.set(cookie)
-    } catch (e) {
-      console.log('cookie', cookie, e)
+    } catch (error) {
+      console.error(`set cookie error, cookie=${cookie}, error=${error}`)
     }
   }
 
-  // cookies.forEach(cookie => {
-  //   session.defaultSession.cookies.set(cookie)
-  // })
-
-  // mainWindow.loadFile(path.join(__dirname, 'index.html'))
   mainWindow.loadURL('https://115.com')
 
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.session.on('will-download', (event, item, webContents) => {
+    const download = {
+      url: item.getURL(),
+      filename: item.getFilename(),
+      progress: `${item.getReceivedBytes() / item.getTotalBytes() * 100}%`,
+      status: '下载中...',
+    }
+    downloads.push(download);
+
+    item.on('updated', (event, state) => {
+      if (state === 'interrupted') {
+        download.status = '已暂停';
+      } else if (state === 'progressing') {
+        if (item.isPaused()) {
+          download.status = '已暂停';
+        } else {
+          download.progress = `${item.getReceivedBytes() / item.getTotalBytes() * 100}%`
+        }
+      }
+    })
+
+    item.once('done', (event, state) => {
+      if (state === 'completed') {
+        download.status = '下载完成'
+      } else {
+        download.status = '下载失败'
+      }
+    })
+  })
 }
 
 app.on('ready', createWindow)
